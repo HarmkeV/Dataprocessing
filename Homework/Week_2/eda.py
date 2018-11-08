ad04934caf2707a4305445ec63d3b1d32aded3e8
HarmkeV <44019003+HarmkeV@users.noreply.github.com>
2018-11-08 12:04:26 +0100

    update

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -1,2 +1,16 @@
 from bs4 import BeautifulSoup
 import csv
+import json
+import pandas
+
+def extract_data(dom):
+    
+
+
+if __name__ == "__main__":
+
+    # get csv content from csv
+    input = simple_get("input.csv")
+
+    # parse the csv file into a DOM representation
+    dom = BeautifulSoup(html, 'input.parser')
