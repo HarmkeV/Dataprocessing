68719e36bbdd0e1b26eb00db3df0114c2e66c335
HarmkeV <44019003+HarmkeV@users.noreply.github.com>
2018-11-21 16:36:46 +0100

    finished

--- Homework/Week_3/createGraph.js
+++ Homework/Week_3/createGraph.js
@@ -27,6 +27,20 @@ function createTransform(domain, range){
 
 var fileName = "data.json";
 var txtFile = new XMLHttpRequest();
+
+// declare variables for x,y positions of graph
+var GRAPH_TOP = 25;
+var GRAPH_BOTTOM = 300;
+var GRAPH_LEFT = 10;
+var GRAPH_RIGHT = 900;
+
+// define values for y axis
+var yAxisValues = [100000000, 110000000, 120000000, 130000000, 140000000];
+
+// start canvas
+var canvas = document.getElementById( "canvas" );
+var context = canvas.getContext( "2d" );
+
 txtFile.onreadystatechange = function() {
     if (txtFile.readyState === 4 && txtFile.status == 200) {
         var data = JSON.parse(txtFile.responseText);
@@ -41,119 +55,124 @@ txtFile.onreadystatechange = function() {
           values.push(data[element]['Projected Emissions - Business As Usual'])
         })
 
-      var canvas = document.getElementById( "canvas" );
-      var context = canvas.getContext( "2d" );
-
-      // declare graph start and end
-      var GRAPH_TOP = 25;
-      var GRAPH_BOTTOM = 300;
-      var GRAPH_LEFT = 10;
-      var GRAPH_RIGHT = 600;
-
-      // clear canvas (if another graph was previously drawn)
-      context.clearRect( 0, 0, 500, 400 );
-
-      // draw X and Y axis
-      context.beginPath();
-      context.moveTo( GRAPH_LEFT, GRAPH_BOTTOM );
-      context.lineTo( GRAPH_RIGHT, GRAPH_BOTTOM );
-      context.lineTo( GRAPH_RIGHT, GRAPH_TOP );
-      context.stroke();
-
-      // call createTransform
-      // find x coordinate
-      domain = [2006, 2020];
-      range = [GRAPH_LEFT, GRAPH_RIGHT];
-
-      xList = [];
-      xCord = createTransform(domain, range);
-      for (var posYear = 0; posYear < 15; posYear++) {
-        xList.push(xCord(years[posYear]));
-      }
-
-      // find y coordinates
-      domain = [100000000, 141000000];
-      range = [GRAPH_BOTTOM, GRAPH_TOP];
-
-      yList = [];
-      yCord = createTransform(domain, range);
-      for (var posValue = 0; posValue < 15; posValue++) {
-        yList.push(yCord(values[posValue]));
-      }
-
-      // find y axis label coordinates
-      domain = [100000000, 141000000];
-      range = [GRAPH_BOTTOM, GRAPH_TOP];
-
-      yaxList = [];
-      var yAxisValues = [100000000, 110000000, 120000000, 130000000, 140000000];
-      yaxCord = createTransform(domain, range);
-      for (var posYAx = 0; posYAx < 5; posYAx++) {
-        yaxList.push(yaxCord(yAxisValues[posYAx]));
-      }
-
-      // draw reference lines
-      context.beginPath();
-      // set light grey color for reference lines
-      context.strokeStyle = "#BBB";
-      context.moveTo( GRAPH_LEFT, GRAPH_TOP );
-      for (posLine = 0; posLine < 5; posLine++) {
-        context.moveTo( GRAPH_LEFT, yaxList[posLine] );
-        context.lineTo( GRAPH_RIGHT, yaxList[posLine] );
-      }
-      context.stroke();
-
-      // create line indicating emissions
-      context.beginPath();
-      context.moveTo(xList[0], yList[0]);
-      for (var posEm = 0; posEm < 15; posEm++) {
-        context.lineTo(xList[posEm], yList[posEm]);
-      }
-      context.strokeStyle = "#4708ce";
-      context.stroke();
-
-      // create ticks for x-axis
-      context.beginPath();
-      context.moveTo( GRAPH_LEFT, GRAPH_BOTTOM );
-      for (var posTickX = 0; posTickX < 15; posTickX++) {
-        context.lineTo( xList[posTickX], GRAPH_BOTTOM + 5);
-        context.moveTo( xList[posTickX + 1], GRAPH_BOTTOM);
-      }
-      context.stroke();
-
-      // create ticks for y-axis
-      context.beginPath();
-      context.moveTo( GRAPH_RIGHT, GRAPH_BOTTOM );
-      for (var posTickY = 0; posTickY < 5; posTickY++) {
-        context.lineTo( GRAPH_RIGHT + 5, yaxList[posTickY]);
-        context.moveTo( GRAPH_RIGHT,  yaxList[posTickY + 1]);
-      }
-      context.stroke();
-
-      // create titles
-      context.fillText("Year", GRAPH_RIGHT / 2, GRAPH_BOTTOM + 50);
-      context.save();
-      context.rotate(-Math.PI / 2);
-      context.fillText("GHG emissions (Mt)", GRAPH_RIGHT + 80, GRAPH_BOTTOM / 2);
-      context.restore();
-      context.save();
-      context.font="bold 16px Arial";
-      context.fillText("Estimated greenhouse gas emissions (Mt) in Montana (2006)", GRAPH_TOP - 15, GRAPH_LEFT + 10);
-      context.restore();
-      context.stroke();
-
-      // draw reference value for x axis
-      for (var refValX = 0; refValX < 15; refValX++) {
-        context.fillText(years[refValX], xList[refValX] - 10, GRAPH_BOTTOM + 15);
-        context.stroke();
-      }
-
-      // draw reference value for y axis
-      for (var refValY = 0; refValY < 5; refValY++) {
-        context.fillText(yAxisValues[refValY], GRAPH_RIGHT + 10, yaxList[refValY]);
-        context.stroke();
-      }
+      var outGr = outlineGraph()
+      var coordinateList = coordinates(values, years)
+      var lines = drawLines(coordinateList)
+      var tick = ticks(coordinateList)
+      layout(coordinateList)
     }
 }
 txtFile.open("GET", fileName);
 txtFile.send();
+
+function outlineGraph(){
+  // clear canvas (if another graph was previously drawn)
+  context.clearRect( 0, 0, 500, 400 );
+
+  // draw X and Y axis
+  context.beginPath();
+  context.moveTo(GRAPH_LEFT, GRAPH_BOTTOM);
+  context.lineTo(GRAPH_RIGHT, GRAPH_BOTTOM);
+  context.lineTo(GRAPH_RIGHT, GRAPH_TOP);
+  context.stroke();
+}
+
+function coordinates(values, years){
+  // call createTransform
+  // find x coordinate
+  domain = [2006, 2020];
+  range = [GRAPH_LEFT, GRAPH_RIGHT];
+
+  xList = [];
+  xCord = createTransform(domain, range);
+  for (var posYear = 0; posYear < 15; posYear++) {
+    xList.push(xCord(years[posYear]));
+  }
+
+  // find y coordinates
+  domain = [100000000, 141000000];
+  range = [GRAPH_BOTTOM, GRAPH_TOP];
+
+  yList = [];
+  yCord = createTransform(domain, range);
+  for (var posValue = 0; posValue < 15; posValue++) {
+    yList.push(yCord(values[posValue]));
+  }
+
+  // find y axis label coordinates
+  domain = [100000000, 141000000];
+  range = [GRAPH_BOTTOM, GRAPH_TOP];
+
+  yaxList = [];
+  yaxCord = createTransform(domain, range);
+  for (var posYAx = 0; posYAx < 5; posYAx++) {
+    yaxList.push(yaxCord(yAxisValues[posYAx]));
+  }
+
+  return xList, yList, yaxList, yAxisValues
+}
+
+function drawLines(coordinateList){
+  /// draw reference lines
+  context.beginPath();
+  // set light grey color for reference lines
+  context.strokeStyle = "#BBB";
+  context.moveTo(GRAPH_LEFT, GRAPH_TOP);
+  for (posLine = 0; posLine < 5; posLine++) {
+    context.moveTo(GRAPH_LEFT, yaxList[posLine]);
+    context.lineTo(GRAPH_RIGHT, yaxList[posLine]);
+  }
+  context.stroke();
+
+  // create line indicating emissions
+  context.beginPath();
+  context.moveTo(xList[0], yList[0]);
+  for (var posEm = 0; posEm < 15; posEm++) {
+    context.lineTo(xList[posEm], yList[posEm]);
+  }
+  context.strokeStyle = "#4708ce";
+  context.stroke();
+}
+
+function ticks(coordinateList){
+  // create ticks for x-axis
+  context.beginPath();
+  context.moveTo(GRAPH_LEFT, GRAPH_BOTTOM);
+  for (var posTickX = 0; posTickX < 15; posTickX++) {
+    context.lineTo(xList[posTickX], GRAPH_BOTTOM + 5);
+    context.moveTo(xList[posTickX + 1], GRAPH_BOTTOM);
+  }
+  context.stroke();
+
+  // create ticks for y-axis
+  context.beginPath();
+  context.moveTo(GRAPH_RIGHT, GRAPH_BOTTOM);
+  for (var posTickY = 0; posTickY < 5; posTickY++) {
+    context.lineTo(GRAPH_RIGHT + 5, yaxList[posTickY]);
+    context.moveTo(GRAPH_RIGHT,  yaxList[posTickY + 1]);
+  }
+  context.stroke();
+}
+
+function layout(coordinateList){
+  // create titles
+  context.fillText("Year", GRAPH_RIGHT / 2, GRAPH_BOTTOM + 50);
+  context.fillText("GHG emissions (Mt)", GRAPH_RIGHT - 31, GRAPH_TOP - 10);
+  context.save();
+  context.font="bold 16px Arial";
+  context.fillText("Estimated greenhouse gas emissions (Mt) in Montana (2006)", GRAPH_TOP - 15, GRAPH_LEFT + 10);
+  context.restore();
+  context.stroke();
+
+  // draw reference value for x axis
+  for (var refValX = 0; refValX < 15; refValX++) {
+    context.fillText(years[refValX], xList[refValX] - 10, GRAPH_BOTTOM + 15);
+    context.stroke();
+  }
+
+  // draw reference value for y axis
+  for (var refValY = 0; refValY < 5; refValY++) {
+    context.fillText(yAxisValues[refValY], GRAPH_RIGHT + 10, yaxList[refValY]);
+    context.stroke();
+  }
+}
