b76ce38fec32f089248ead8960872df008f32379
HarmkeV <44019003+HarmkeV@users.noreply.github.com>
2018-12-10 15:06:49 +0100

    goede mapsize

--- Homework/Week_6/linkedview.js
+++ Homework/Week_6/linkedview.js
@@ -4,15 +4,27 @@ Harmke Vliek
 script for D3 linked views
 */
 
+//  svg variables
+var height = 400
+
 window.onload = function() {
   // show map of the Netherlands, imported from http://jvectormap.com/maps/countries/netherlands/
-  $('#map').vectorMap({map: 'nl_merc'});
+  $('#map').vectorMap({map: 'nl_merc', focusOn: {
+       x: 0.6,
+       y: -0.2,
+       scale: 9
+     }});
+
+  // set height of svg
+  d3.select(".jvectormap-container")
+    .select("svg")
+    .attr("height", height)
 
   //load data from data.json
-  d3.json('data.json').then(function(data) {
+  d3.json('dataCBS.json').then(function(data) {
      var province = Object.keys(data);
      var values = Object.values(data);
-     console.log(province)
-     console.log(values)
-});
+  });
+
+
 };
