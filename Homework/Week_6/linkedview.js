5084333a60ac1eab01633bb58891989ed207e12a
HarmkeV <44019003+HarmkeV@users.noreply.github.com>
2018-12-10 14:36:17 +0100

    kleine kaart

--- Homework/Week_6/linkedview.js
+++ Homework/Week_6/linkedview.js
@@ -3,3 +3,16 @@ Harmke Vliek
 10989137
 script for D3 linked views
 */
+
+window.onload = function() {
+  // show map of the Netherlands, imported from http://jvectormap.com/maps/countries/netherlands/
+  $('#map').vectorMap({map: 'nl_merc'});
+
+  //load data from data.json
+  d3.json('data.json').then(function(data) {
+     var province = Object.keys(data);
+     var values = Object.values(data);
+     console.log(province)
+     console.log(values)
+});
+};
